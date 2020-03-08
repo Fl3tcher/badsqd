e4d68a14308912e4021f8d482a4ed92d7f8a7965
Fl3tcher <60181373+Fl3tcher@users.noreply.github.com>
2020-03-08 10:50:38 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -56,10 +56,10 @@ async def on_message(message):
         if message.author.permissions_in(message.channel).manage_messages:
             args = message.content.split(' ')
             if len(args) == 2:
-                if args[1].isdigit():1
+                if args[1].isdigit():
                     count = int(args[1]) + 1
                     deleted = await message.channel.purge(limit=count, check=is_not_pinned)
-                    await message.channel.send('{} сообщений удалено.'.format(len(deleted)-1))                              
+                    await message.channel.send('{} сообщений удалено.'.format(len(deleted)-1))                               
 
 
 token = os.environ.get('BOT_TOKEN')
