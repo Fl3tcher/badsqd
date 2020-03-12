519cd2cb90c5b2659038238097c254897c41c6d1
Fl3tcher <60181373+Fl3tcher@users.noreply.github.com>
2020-03-12 16:47:25 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -30,11 +30,13 @@ async def on_message(message):
     if '.help' in message.content:  
         await message.channel.send('')
     if message.content.startswith('.link'):
-            embed=discord.Embed(title="discord.gg", url="https://discordapp.com/oauth2/authorize?&client_id=669249748909162513&scope=bot&permissions=26624")
-            embed.set_author(name="Links")
-            embed.set_thumbnail(url="https://i.imgur.com/SbmwC1T.jpg")
-            embed.set_footer(text="Developer : Wizel")
-                await message.channel.send(embed=embed)      
+
+        embed=discord.Embed(title="discord.gg", 
+                            url="https://discordapp.com/oauth2/authorize?&client_id=669249748909162513&scope=bot&permissions=26624", 
+                            color=0x8080ff)
+        embed.set_thumbnail(url="https://i.imgur.com/SbmwC1T.jpg")
+        embed.set_footer(text="Developer : Wizel\")
+        await self.bot.say(embed=embed)   
     if message.content.startswith('.stats'):
         args = message.content.split(' ')
         if len(args) == 2:
