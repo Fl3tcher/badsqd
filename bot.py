c329da5a4ea4c7ec8d44169b075082ed637bf1ae
Fl3tcher <60181373+Fl3tcher@users.noreply.github.com>
2020-03-22 13:37:04 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -30,12 +30,14 @@ async def on_message(message):
     if 'commands' in message.content:  
         await message.channel.send('')
     if message.content.startswith('.help'): # Help on commands in the bot
-        embed=discord.Embed(description='Для получения дополнительной информации о конкретной команде используйте .help {command}')
-        embed.set_author(name="Commands")
+        embed=discord.Embed(description='Для получения дополнительной информации о конкретной команде используйте `.help {command}`')
+        embed.set_author(name="# Commands")
+        embed.add_field(name='Основные',
+                        value='`stats` `link`')
         embed.add_field(name='Модерирование',
                         value='`mute` `ban` `clear`')
         mess = await message.channel.send(embed=embed)
-    if message.content.startswith('.user'): # User statistics on the server
+    if message.content.startswith('.stats'): # User statistics on the server
         args = message.content.split(' ')
         if len(args) == 2:
             member: Member = discord.utils.find(lambda m: args[1] in m.name, message.guild.members)
