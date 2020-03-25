07fa3a9828299c458e651fe249349b47e677cee8
Fl3tcher <60181373+Fl3tcher@users.noreply.github.com>
2020-03-26 02:31:43 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -37,6 +37,12 @@ async def on_message(message):
         embed.add_field(name='Модерирование',
                         value='`mute` `ban` `clear`')
         mess = await message.channel.send(embed=embed)
+            if message.content.startswith('.help stats'):
+                e
+                embed=discord.Embed(description='Хотите подключить бота на свой сервер дискорд? Вам пожет данная команда: `.link`')
+                embed.set_author(name="& Links")
+                mess = await message.channel.send(embed=embed)
+               
     if message.content.startswith('.stats'): # User statistics on the server
         args = message.content.split(' ')
         if len(args) == 2:
